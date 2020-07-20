b71292046975c846216104ae45f8145dd0ddda13
Derek Stampone <dstampone@schools.nyc.gov>
2020-07-19 22:24:01 -0400

    Update template

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ from flask import render_template
 from flask import request
 from flask import session
 
-import requests
+import requests #To access our API
 
 # -- Initialization section --
 app = Flask(__name__)
 
+## secret key for session (In production, you would set this key via an environment variable)
+app.secret_key = b'HO\xf8\xff+\n\x1e\\~/;}'
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template('index.html')
